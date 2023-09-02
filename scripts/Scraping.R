8c4d314de81e5d084158dd7947e51fb6a6073380
Luis-Borda <142829063+Luis-Borda@users.noreply.github.com>
2023-09-02 16:47:44 -0500

    Update Scraping.R

---
+++ scripts/Scraping.R
@@ -0,0 +1,10 @@
+################################################################################
+############TALLER 1 
+#SCRAPING
+
+## llamar la librería pacman: contiene la función p_load()
+require(pacman)
+
+## p_load llama/instala-llama las librerías que se enlistan:
+p_load(tidyverse, # contiene las librerías ggplot, dplyr...
+       rvest) # web-scraping
