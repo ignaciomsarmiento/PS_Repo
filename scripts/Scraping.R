ed538d0dd8e2ce58a00686c909da2b16bffa5690
Luis-Borda <142829063+Luis-Borda@users.noreply.github.com>
2023-09-05 23:20:19 -0500

    Update Scraping.R

--- scripts/Scraping.R
+++ scripts/Scraping.R
@@ -1,12 +1,26 @@
 ################################################################################
 ############TALLER 1 
 #SCRAPING
+#Limpieza de area de trabajo --------------------------------------------
+
+rm(list = ls())
 
 ## llamar la librería pacman: contiene la función p_load()
 require(pacman)
 
 ## p_load llama/instala-llama las librerías que se enlistan:
-p_load(tidyverse, # contiene las librerías ggplot, dplyr...
-       rvest) # web-scraping
+library(pacman)
 
-##
+p_load(rio,
+       tidyverse,
+       skimr,
+       caret,
+       readxl,
+       rvest,
+       stargazer,
+       knitr,
+       boot,
+       data.table)
+
+
+##x
