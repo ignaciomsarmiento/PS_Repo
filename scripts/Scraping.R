44414d918bff78d1ee1f63cbe9f48044b4474677
Luis-Borda <142829063+Luis-Borda@users.noreply.github.com>
2023-09-06 22:16:29 -0500

    Scraping y generación de base de datos

--- scripts/Scraping.R
+++ scripts/Scraping.R
@@ -5,11 +5,12 @@
 
 rm(list = ls())
 
+
+setwd("C:\Users\lordb\OneDrive - Universidad de los andes\2. GRUPOS\2. BIG DATA\PS_Repo_Taller1_G10")
+
 ## llamar la librería pacman: contiene la función p_load()
 require(pacman)
 
-## p_load llama/instala-llama las librerías que se enlistan:
-library(pacman)
 
 p_load(rio,
        tidyverse,
@@ -23,4 +24,24 @@ p_load(rio,
        data.table)
 
 
-##x
+##Se genera un vector con el link y sus bases de datos
+
+link <- paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 1:10, ".html")
+
+
+#Se agrupan las bases de datos 
+
+base <- data.frame()
+for(i in link){
+  print(i)
+  GEIH<- read_html(i) %>% 
+    html_table() %>%
+    as.data.frame()
+  base <- rbind(base, GEIH)
+}
+
+View(base)
+
+#Exportamos la base 
+
+write.csv(base, "GEIH.csv")
