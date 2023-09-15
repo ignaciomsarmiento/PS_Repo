0c1978fb742cbd58c867f94b46e8223964b6d9ad
Luis-Borda <142829063+Luis-Borda@users.noreply.github.com>
2023-09-15 18:05:17 -0500

    Update Limpieza_Datos.R

--- scripts/Limpieza_Datos.R
+++ scripts/Limpieza_Datos.R
@@ -3,7 +3,14 @@
 ###Limpieza de los datos 
 
 #SelecciOnamos las variables de interés para el modelo que se quiere estimar
+p_load(rio, #import/export data
+       tidyverse, #tidy-data
+       skimr, #sumary data
+       caret, #clasification and regreation trading)
 
+library(readr)
+GEIH <- read_csv("C:/Users/lordb/OneDrive - Universidad de los andes/2. GRUPOS/2. BIG DATA/PS_Repo_Taller1_G10/stores/GEIH.csv")
+View(GEIH)
 
 
 dt_total <- base %>% 
