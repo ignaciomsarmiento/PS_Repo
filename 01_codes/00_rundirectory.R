8f51e50faf2916087ee037425b8e4f8e3b80e207
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2026-01-21 19:43:13 +0100

    Update 00_rundirectory.R

--- 01_codes/00_rundirectory.R
+++ 01_codes/00_rundirectory.R
@@ -7,8 +7,8 @@
 
 
 # This script gets the data, there are no restrictions to getting the data
-source("01_get_data.R")
+source("01_code/01_get_data.R")
 
 # This script imputes missing values and saves a db in a temporary folder 03_temp called data_with_imputed_Values.Rds
-source("02_clean_data.R")
+source("01_code/02_clean_data.R")
 
