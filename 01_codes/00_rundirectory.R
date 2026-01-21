02270edbd0edb0f9f7e58c4000f121594e40f77c
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2026-01-21 19:42:43 +0100

    Create 00_rundirectory.R

---
+++ 01_codes/00_rundirectory.R
@@ -0,0 +1,14 @@
+
+##########################################################
+# Replicates results for XXX
+# authors: 
+# Instructions: To reproduce all results, run: source("01_code/00_rundirectory.R")
+##########################################################
+
+
+# This script gets the data, there are no restrictions to getting the data
+source("01_get_data.R")
+
+# This script imputes missing values and saves a db in a temporary folder 03_temp called data_with_imputed_Values.Rds
+source("02_clean_data.R")
+
