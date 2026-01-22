1f2505e7233dd0e1e378d6d42efd228b5e1b9940
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2026-01-22 09:27:17 +0100

    Run directory mas didáctico

--- 01_code/00_rundirectory.R
+++ 01_code/00_rundirectory.R
@@ -1,14 +1,27 @@
-
 ##########################################################
-# Replicates results for XXX
-# authors: 
-# Instructions: To reproduce all results, run: source("01_code/00_rundirectory.R")
+# Master script
+#
+# Running this file reproduces all results in the repository.
+#
+# To reproduce all results, run:
+# from an interactive R session: source("01_code/00_rundirectory.R")   
+# or from the command line: R CMD BATCH 01_code/00_rundirectory.R
+#
+# Authors:
+# - Ignacio Sarmiento-Barbieri
+# - Gustavo Castillo Alvarez
+##########################################################
+# NOTE:
+# - This file is provided as an EXAMPLE
+# - This script should only call other scripts.
 ##########################################################
 
-
-# This script gets the data, there are no restrictions to getting the data
+# Step 1: Download and construct the raw dataset
 source("01_code/01_get_data.R")
 
-# This script imputes missing values and saves a db in a temporary folder 03_temp called data_with_imputed_Values.Rds
-source("01_code/02_clean_data.R")
+# Step 2: Filters data 
+# The filtered dataset is saved to a temporary folder for downstream use
+source("01_code/02_filters_data.R")
+
+
 
